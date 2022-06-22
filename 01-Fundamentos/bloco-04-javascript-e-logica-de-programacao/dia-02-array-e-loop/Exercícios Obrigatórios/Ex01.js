1e40ca4be3a48f812745871bb701fc77f1e914e8
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-06-22 15:32:35 -0300

    Realizei 3 exercícios

---
+++ 01-Fundamentos/bloco-04-javascript-e-logica-de-programacao/dia-02-array-e-loop/Exercícios Obrigatórios/Ex01.js
@@ -0,0 +1,38 @@
+// 1- Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   console.log (numbers[index])
+// }
+
+
+// 2- Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let soma = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   soma += numbers[index];
+
+// }
+// console.log (soma)
+
+
+// 3- Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let soma = 0;
+// let div = 0;
+
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   soma += numbers[index];
+//   div = soma / index
+// }
+// console.log (div)
+
+
+// 4- Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
