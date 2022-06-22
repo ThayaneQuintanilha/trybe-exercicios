7c9d7bd9bc1a22a77e084a234184c37e4a9d7d1c
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-06-22 15:36:10 -0300

    Exercício 4 concluido

--- 01-Fundamentos/bloco-04-javascript-e-logica-de-programacao/dia-02-array-e-loop/Exercícios Obrigatórios/Ex01.js
+++ 01-Fundamentos/bloco-04-javascript-e-logica-de-programacao/dia-02-array-e-loop/Exercícios Obrigatórios/Ex01.js
@@ -36,3 +36,44 @@
 
 
 // 4- Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let soma = 0;
+// let div = 0;
+
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   soma += numbers[index];
+//   div = soma / index
+// }
+
+// if (div > 20) {
+//   console.log ("valor maior que 20")
+// } else {
+//   console.log ("valor menor ou igual a 20")
+// }
+
+// console.log (div)
+
+
+// 5- 🚀 Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+let div = 0;
+
+
+for (let index = 0; index < numbers.length; index += 1) {
+  soma += numbers[index];
+  div = soma / index
+}
+
+if (div > 20) {
+  console.log ("valor maior que 20")
+} else {
+  console.log ("valor menor ou igual a 20")
+}
+
+console.log (div)
