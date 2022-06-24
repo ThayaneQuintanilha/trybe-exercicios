25c4fb3d67158441aa991b4f57bef5e34836dc05
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-06-24 15:39:35 -0300

    Exercício de for in feito

---
+++ 01-Fundamentos/bloco-04-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/for in.js
@@ -0,0 +1,19 @@
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge',
+};
+
+for (let names2 in names) {
+  console.log(names[names2]);
+}
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (let key in car) {
+  console.log(key, car[key]);
+}
