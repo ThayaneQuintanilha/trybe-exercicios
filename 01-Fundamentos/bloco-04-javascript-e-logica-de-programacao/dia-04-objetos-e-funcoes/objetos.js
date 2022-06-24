6732ae6b144dff25b2ac04275d0d5b96d2cb08d5
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-06-24 15:29:01 -0300

    Exercício de objetos feito

---
+++ 01-Fundamentos/bloco-04-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/objetos.js
@@ -0,0 +1,20 @@
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };
+
+let player = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {
+    goden: 2,
+    silver: 3,
+  },
+};
+
+// console.log(player.name);
+// console.log(player.lastName);
+// console.log(player.age);
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
