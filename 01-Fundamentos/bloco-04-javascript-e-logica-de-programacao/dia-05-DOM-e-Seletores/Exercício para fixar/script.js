a15e1fc07642f690de333c3e60265ab38c917a15
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-06-28 18:33:19 -0300

    Exercícios para Fixar feitos 1 ao 3

---
+++ 01-Fundamentos/bloco-04-javascript-e-logica-de-programacao/dia-05-DOM-e-Seletores/Exercício para fixar/script.js
@@ -0,0 +1,8 @@
+// - 1
+document.getElementsByTagName('p')[1].innerText = 'Em 2 gostaria de estar em uma grande empresa, continuar aprendendo outras linguagem e frameworks.';
+
+// - 2
+document.getElementsByClassName('main-content')[0].style = 'background-color: green;'
+
+// - 3
+document.getElementsByClassName('center-content')[0].style = 'background-color: white'
