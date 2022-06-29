1732e0cb37aa637b4ab9613ceb3c37ec17b1e32d
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-06-29 16:10:37 -0300

    Parte 1 do exercício dos vídeos

---
+++ 01-Fundamentos/bloco-04-javascript-e-logica-de-programacao/dia-06-trabalhando-com-elementos/Parte I/script.js
@@ -0,0 +1,16 @@
+// - 1
+document.getElementById('elementoOndeVoceEsta');
+// - 2
+document.getElementById('pai').parentElement.style = 'color: blue;'
+// - 3
+document.getElementById('primeiroFilhoDoFilho').innerText = 'Adicionei'
+// - 4
+document.getElementById('pai').firstElementChild
+// - 5
+document.getElementById('elementoOndeVoceEsta').previousElementSibling;
+// - 6
+document.getElementById('elementoOndeVoceEsta').nextSibling
+// - 7
+document.getElementById('elementoOndeVoceEsta').nextElementSibling
+// - 8
+document.getElementById('pai').lastElementChild.previousElementSibling
