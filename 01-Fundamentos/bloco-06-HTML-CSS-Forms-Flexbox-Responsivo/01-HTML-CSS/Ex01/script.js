df2aa1a52649014583c555adb882a0e38f31a247
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-07-06 15:56:23 -0300

    Enviando exercícios do dia

---
+++ 01-Fundamentos/bloco-06-HTML-CSS-Forms-Flexbox-Responsivo/01-HTML-CSS/Ex01/script.js
@@ -0,0 +1,20 @@
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+HREF_LINK.addEventListener("click", function (event){
+  event.preventDefault()
+})
+
+INPUT_CHECKBOX.addEventListener("click", function(event){
+  event.preventDefault()
+})
+
+INPUT_TEXT.addEventListener('keypress', function(event){
+  if(event.key === 'a'){
+      INPUT_TEXT = input.value
+  }
+  else {
+      event.preventDefault();
+  }
+})
