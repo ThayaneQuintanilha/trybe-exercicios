801049b163f79114fa1d1cf2dc45a88dced4249d
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-07-06 19:08:14 -0300

    Exercícios feitos

--- 01-Fundamentos/bloco-06-HTML-CSS-Forms-Flexbox-Responsivo/01-HTML-CSS/Ex01/script.js
+++ 01-Fundamentos/bloco-06-HTML-CSS-Forms-Flexbox-Responsivo/01-HTML-CSS/Ex01/script.js
@@ -1,20 +1,28 @@
-const INPUT_TEXT = document.querySelector("#input-text");
-const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
-const HREF_LINK = document.querySelector("#href");
+// const INPUT_TEXT = document.querySelector("#input-text");
+// const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+// const HREF_LINK = document.querySelector("#href");
 
-HREF_LINK.addEventListener("click", function (event){
+// HREF_LINK.addEventListener("click", function (event){
+//   event.preventDefault()
+// })
+
+// INPUT_CHECKBOX.addEventListener("click", function(event){
+//   event.preventDefault()
+// })
+
+// INPUT_TEXT.addEventListener('keypress', function(event){
+//   if(event.key === 'a'){
+//       INPUT_TEXT = input.value
+//   }
+//   else {
+//       event.preventDefault();
+//   }
+// })
+
+//------- 2 ---------
+
+const submitButton = document.querySelector('#submit-button');
+
+submitButton.addEventListener("click", function(event){
   event.preventDefault()
-})
-
-INPUT_CHECKBOX.addEventListener("click", function(event){
-  event.preventDefault()
-})
-
-INPUT_TEXT.addEventListener('keypress', function(event){
-  if(event.key === 'a'){
-      INPUT_TEXT = input.value
-  }
-  else {
-      event.preventDefault();
-  }
 })
