6afd625ebe39b0d1d72029c3c8b1990a8ffb316c
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-07-14 14:58:21 -0300

    exercício 1 da parte I

--- 01-Fundamentos/bloco-07-JavascriptES6-e-Testes-Unitários/script.js
+++ 01-Fundamentos/bloco-07-JavascriptES6-e-Testes-Unitários/script.js
@@ -1,23 +1,23 @@
-function userInfo() {
-  let userEmail = 'maria@email.com';
+// function userInfo() {
+//   let userEmail = 'maria@email.com';
 
-  // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
-  console.log(userEmail);
-}
-userInfo();
+//   // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
+//   console.log(userEmail);
+// }
+// userInfo();
 
-if (true) {
-  // inicio do escopo do if
-  var userAge = '20';
-  console.log(userAge); // 20
-  // fim do escopo do if
-}
-console.log(userAge); // 20
+// if (true) {
+//   // inicio do escopo do if
+//   var userAge = '20';
+//   console.log(userAge); // 20
+//   // fim do escopo do if
+// }
+// console.log(userAge); // 20
 
-let favoriteLanguage = 'Javascript';
-favoriteLanguage = 'Python';
-console.log(favoriteLanguage); // Erro
+// let favoriteLanguage = 'Javascript';
+// favoriteLanguage = 'Python';
+// console.log(favoriteLanguage); // Erro
 
-let favoriteTechnology = 'Machine learning';
-favoriteTechnology = 'Facial recognition';
-console.log(favoriteTechnology); // Facial recognition
+// let favoriteTechnology = 'Machine learning';
+// favoriteTechnology = 'Facial recognition';
+// console.log(favoriteTechnology); // Facial recognition
