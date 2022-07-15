54404ab4102f245cdd7ba27353720f4b89fd30af
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-07-15 15:40:01 -0300

    exercícios e conteudos

--- 01-Fundamentos/bloco-07-JavascriptES6-e-Testes-Unitários/dia-02-fluxo-de-exerção-e-objetos/script.js
+++ 01-Fundamentos/bloco-07-JavascriptES6-e-Testes-Unitários/dia-02-fluxo-de-exerção-e-objetos/script.js
@@ -40,16 +40,55 @@
 // customer2['lastName'] = 'Silva';
 // console.log(customer2);
  
-const customer = {
-  firstName: 'Roberto',
-  age: 22,
-  job: 'Teacher',
-};
+// const customer = {
+//   firstName: 'Roberto',
+//   age: 22,
+//   job: 'Teacher',
+// };
 
-let newKey = 'lastName';
-const lastName = 'Ferreira';
-customer[newKey] = lastName;
-newKey = 'fullName';
-const fullName = `${customer.firstName} ${customer.lastName}`;
-customer[newKey] = fullName;
-console.log(customer);
+// let newKey = 'lastName';
+// const lastName = 'Ferreira';
+// customer[newKey] = lastName;
+// newKey = 'fullName';
+// const fullName = `${customer.firstName} ${customer.lastName}`;
+// customer[newKey] = fullName;
+// console.log(customer);
+
+// const student = {
+//   Html: 'Muito Bom',
+//   Css: 'Bom',
+//   JavaScript: 'Ótimo',
+//   SoftSkill: 'Ótimo',
+// };
+
+// const listSkillsValuesWithFor = (student) => {
+//   const skills = [];
+//   for(skill in student) {
+//     skills.push(student[skill]);
+//   }
+
+//   return skills;
+// };
+
+// const listSkillsValuesWithValues = (student) => Object.values(student);
+
+// Sem Object.values
+// console.log(listSkillsValuesWithFor(student));
+
+// // Com Object.values
+// console.log(listSkillsValuesWithValues(student));
+
+// const países = {
+//   França: 'Paris',
+//   Brasil: 'Brasília',
+//   Espanha: 'Madrid',
+//   Portugal: 'Lisboa',
+// };
+// const pairKeyValue = Object.entries(países);
+// console.log(pairKeyValue);
+
+// for(index in pairKeyValue) {
+//   console.log('--------');
+//   console.log('País:', pairKeyValue[index][0]);
+//   console.log('Capital:', pairKeyValue[index][1]);
+// };
