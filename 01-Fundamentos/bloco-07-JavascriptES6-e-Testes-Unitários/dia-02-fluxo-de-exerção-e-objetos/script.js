afaf08ceac19eb50294ef1cbf6a91f61855d8f8c
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-07-15 15:15:08 -0300

    exercício 2

--- 01-Fundamentos/bloco-07-JavascriptES6-e-Testes-Unitários/dia-02-fluxo-de-exerção-e-objetos/script.js
+++ 01-Fundamentos/bloco-07-JavascriptES6-e-Testes-Unitários/dia-02-fluxo-de-exerção-e-objetos/script.js
@@ -1,21 +1,55 @@
-const sum = (value1, value2) => value1 + value2;
-console.log(sum(2,3));
+// const sum = (value1, value2) => value1 + value2;
+// console.log(sum(2,3));
 
-const verifyIsNumber = (value1, value2) => {
-  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-    throw new Error('Os valores devem ser numéricos');
-  }
+// const verifyIsNumber = (value1, value2) => {
+//   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+//     throw new Error('Os valores devem ser numéricos');
+//   }
+// };
+
+// const soma = (value1, value2) => {
+//   try {
+//     verifyIsNumber(value1,value2);
+//     return value1 + value2
+//   }
+//   catch (erro) {
+//     return erro.message;
+//   }
+// }
+
+// console.log(soma(5, 5));
+
+// const customer1 = {
+//   firstName: 'Roberto',
+//   age: 22,
+//   job: 'Teacher',
+// };
+
+// console.log(customer1);
+
+// customer1.lastName = 'Faria';
+// console.log(customer1);
+
+// const customer2 = {
+//   firstName: 'Maria',
+//   age: 23,
+//   job: 'Medic',
+// };
+
+// console.log(customer2);
+// customer2['lastName'] = 'Silva';
+// console.log(customer2);
+ 
+const customer = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
 };
 
-const soma = (value1, value2) => {
-  try {
-    verifyIsNumber(value1,value2);
-    return value1 + value2
-  }
-  catch (erro) {
-    return erro.message;
-  }
-}
-
-console.log(soma(5, 5));
-
+let newKey = 'lastName';
+const lastName = 'Ferreira';
+customer[newKey] = lastName;
+newKey = 'fullName';
+const fullName = `${customer.firstName} ${customer.lastName}`;
+customer[newKey] = fullName;
+console.log(customer);
