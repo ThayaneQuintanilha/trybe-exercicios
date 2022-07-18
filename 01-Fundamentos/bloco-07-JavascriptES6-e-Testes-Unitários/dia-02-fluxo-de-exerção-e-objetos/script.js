ba52039c1e52febcf8ce7ce7cdb7f5f24fa367ab
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-07-18 12:09:25 -0300

    exercício de sexta concluído

--- 01-Fundamentos/bloco-07-JavascriptES6-e-Testes-Unitários/dia-02-fluxo-de-exerção-e-objetos/script.js
+++ 01-Fundamentos/bloco-07-JavascriptES6-e-Testes-Unitários/dia-02-fluxo-de-exerção-e-objetos/script.js
@@ -92,3 +92,35 @@
 //   console.log('País:', pairKeyValue[index][0]);
 //   console.log('Capital:', pairKeyValue[index][1]);
 // };
+
+// const person = {
+//   name: 'Alberto',
+//   lastName: 'Gomes',
+//   age: 20,
+// };
+
+// const info = {
+//   age: 23,
+//   job: 'engenheiro',
+// };
+
+// const family = {
+//   children: ['Maria', 'João'],
+//   wife: 'Ana',
+// };
+
+// Object.assign(person, info, family)
+// console.log(person)
+
+// const person = {
+//   name:'Roberto',
+// };
+
+// const lastName = {
+//   lastName: 'Silva',
+// };
+
+// const newPerson = Object.assign({},person,lastName);
+// newPerson.name = 'Gilberto';
+// console.log(newPerson);
+// console.log(person);
