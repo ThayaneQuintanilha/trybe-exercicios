47b113780dacb63bf498b2cff20da8a9c741ae90
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-07-25 16:06:41 -0300

    Exercício 2 dos vídeos

--- bloco-08-HOFS/dia-03-map-filter/script.js
+++ bloco-08-HOFS/dia-03-map-filter/script.js
@@ -6,12 +6,23 @@
 // console.log(newListStudent);
 
 
-const persons = [
-  { firstName: 'Maria', lastName: 'Ferreira' },
-  { firstName: 'João', lastName: 'Silva' },
-  { firstName: 'Antonio', lastName: 'Cabral' },
-];
+// const persons = [
+//   { firstName: 'Maria', lastName: 'Ferreira' },
+//   { firstName: 'João', lastName: 'Silva' },
+//   { firstName: 'Antonio', lastName: 'Cabral' },
+// ];
 
-const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`)
+// const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`)
 
-console.log(fullNames);
+// console.log(fullNames);
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listaProdutos, listaPrecos) => listaProdutos.map((produtos, indice) => {
+  return {[produtos]: listaPrecos[indice]}
+})
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
