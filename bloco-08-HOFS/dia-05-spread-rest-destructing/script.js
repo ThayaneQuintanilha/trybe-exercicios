9b7db2c69f8f9d37886153506c5a6399336d3c35
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-07-27 12:54:07 -0300

    Exercício da prática 2 feito

--- bloco-08-HOFS/dia-05-spread-rest-destructing/script.js
+++ bloco-08-HOFS/dia-05-spread-rest-destructing/script.js
@@ -11,21 +11,59 @@
 // console.log(fruitSalad(specialFruit, additionalItens));
 
 // definindo o objeto
-const character = {
-  name: 'Luke SkyWalker',
-  age: '53',
-  description: {
-    specieName: 'Human',
-    jedi: true,
-  },
-  homeWorld: {
-    name: 'Tatooine',
-    population: '200000',
-  },
+// const character = {
+//   name: 'Luke SkyWalker',
+//   age: '53',
+//   description: {
+//     specieName: 'Human',
+//     jedi: true,
+//   },
+//   homeWorld: {
+//     name: 'Tatooine',
+//     population: '200000',
+//   },
+// };
+
+// // desestruturando o objeto:
+// const { name, age, homeWorld: { name: planetName }, description: { jedi } } = character
+
+// // imprimindo os valores:
+// console.log(`Esse é o ${name}, ele tem ${age} anos, mora no planeta ${planetName} e, por incrível que possa parecer, ele ${jedi ? 'é um Jedi' : 'não é um Jedi'}.`);
+
+// const daysOfWeek = {
+//   workDays: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+//   weekend: ['Saturday', 'Sunday'],
+// };
+
+// const { workDays, weekend } = daysOfWeek;
+
+// console.log(workDays, weekend);
+
+// const student = {
+//   a: 'Maria',
+//   b: 'Turma B',
+//   c: 'Matematica',
+// };
+
+// const { a: nome, b: turma, c: materia } = student;
+
+// console.log(nome, turma, materia);
+
+
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
 };
 
-// desestruturando o objeto:
-const { name, age, homeWorld: { name: planetName }, description: { jedi } } = character
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
 
-// imprimindo os valores:
-console.log(`Esse é o ${name}, ele tem ${age} anos, mora no planeta ${planetName} e, por incrível que possa parecer, ele ${jedi ? 'é um Jedi' : 'não é um Jedi'}.`);
+const { name, age, nationality } = user;
+const { profession, squad, squadInitials } = jobInfos
+
+const result = `"Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`
+console.log(result);
