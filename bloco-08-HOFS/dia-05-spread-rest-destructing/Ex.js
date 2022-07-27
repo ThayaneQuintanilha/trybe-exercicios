067aa4c1cb228c5046c67a1e2082668c778ad8ad
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-07-27 18:57:40 -0300

    Exercício 3 feito

--- bloco-08-HOFS/dia-05-spread-rest-destructing/Ex.js
+++ bloco-08-HOFS/dia-05-spread-rest-destructing/Ex.js
@@ -14,9 +14,32 @@
 
 // 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
 
-const sum = (...numbers) => numbers
-.reduce((acumulador, valorTotal) => {
-  return acumulador + valorTotal
-}, 0)
+// const sum = (...numbers) => numbers
+// .reduce((acumulador, valorTotal) => {
+//   return acumulador + valorTotal
+// }, 0)
 
-console.log(sum(3,5,7,9,12,5));
+// console.log(sum(3,5,7,9,12,5));
+
+// 3 - Escreva a função personLikes, que dado um objeto de parâmetro que representa uma pessoa, retorna todos os gostos daquela pessoa, conforme mostrado abaixo:
+// Dica: use object destructuring.
+
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+// complete a assinatura da função abaixo
+const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+
+console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
