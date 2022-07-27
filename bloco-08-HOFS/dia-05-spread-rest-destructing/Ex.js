242b8803cb9afe3d0c5968609093aa3bf9ff81f8
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-07-27 18:54:38 -0300

    Exercício 2 feito

--- bloco-08-HOFS/dia-05-spread-rest-destructing/Ex.js
+++ bloco-08-HOFS/dia-05-spread-rest-destructing/Ex.js
@@ -1,13 +1,22 @@
 // 1 - Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2, 15 e 54.
 
-const rectangleArea = (width, height) => width * height;
+// const rectangleArea = (width, height) => width * height;
 
-const rectangle1 = [1, 2];
-const rectangle2 = [3, 5];
-const rectangle3 = [6, 9];
-const rectangles = [rectangle1, rectangle2, rectangle3];
+// const rectangle1 = [1, 2];
+// const rectangle2 = [3, 5];
+// const rectangle3 = [6, 9];
+// const rectangles = [rectangle1, rectangle2, rectangle3];
 
-rectangles.forEach((rectangle) => {
-  rectangleArea(...rectangle)
-  console.log(rectangle[0] * rectangle[1]);
-});
+// rectangles.forEach((rectangle) => {
+//   rectangleArea(...rectangle)
+//   console.log(rectangle[0] * rectangle[1]);
+// });
+
+// 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
+
+const sum = (...numbers) => numbers
+.reduce((acumulador, valorTotal) => {
+  return acumulador + valorTotal
+}, 0)
+
+console.log(sum(3,5,7,9,12,5));
