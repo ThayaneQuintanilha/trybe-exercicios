81fc11b990e9d97604abfd66eab8df2e44a4b2ad
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-07-27 19:12:28 -0300

    Exercício 4 feito

--- bloco-08-HOFS/dia-05-spread-rest-destructing/Ex.js
+++ bloco-08-HOFS/dia-05-spread-rest-destructing/Ex.js
@@ -24,22 +24,59 @@
 // 3 - Escreva a função personLikes, que dado um objeto de parâmetro que representa uma pessoa, retorna todos os gostos daquela pessoa, conforme mostrado abaixo:
 // Dica: use object destructuring.
 
-const alex = {
-  name: 'Alex',
-  age: 26,
-  likes: ['fly fishing'],
-  nationality: 'Australian',
-};
+// const alex = {
+//   name: 'Alex',
+//   age: 26,
+//   likes: ['fly fishing'],
+//   nationality: 'Australian',
+// };
 
-const gunnar = {
-  name: 'Gunnar',
-  age: 30,
-  likes: ['hiking', 'scuba diving', 'taking pictures'],
-  nationality: 'Icelandic',
-};
+// const gunnar = {
+//   name: 'Gunnar',
+//   age: 30,
+//   likes: ['hiking', 'scuba diving', 'taking pictures'],
+//   nationality: 'Icelandic',
+// };
 
-// complete a assinatura da função abaixo
-const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+// // complete a assinatura da função abaixo
+// const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
 
-console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
-console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+// console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+// console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
+
+// 🚀 4 - Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+// Dica: use object destructuring.
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// escreva filterPeople abaixo
+const filterPeople = (obj) => obj
+.filter(({ bornIn, nationality }) => nationality === 'Australian' && bornIn >= 1901 && bornIn <= 2000);
+
+console.log(filterPeople(people));
