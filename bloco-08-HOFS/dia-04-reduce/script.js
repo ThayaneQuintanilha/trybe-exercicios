dd1389069e018275b202e66a9665debfe743d40b
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-07-27 16:11:03 -0300

    Exercício reduce

--- bloco-08-HOFS/dia-04-reduce/script.js
+++ bloco-08-HOFS/dia-04-reduce/script.js
@@ -1,3 +1,19 @@
-const totalPrice = items.reduce((total, item) => {
-  return total + item.price
-}, 0)
+// const totalPrice = items.reduce((total, item) => {
+//   return total + item.price
+// }, 0)
+
+const women = [
+  { fullName: 'Margaret Hamilton', email: 'magarethamilton@mulheres.br' },
+  { fullName: 'Larissa Machado', email: 'anitta@mulheres.com.br' },
+  { fullName: 'Frida Kahlo', email: 'fdKahlo@mulheres.br' },
+  { fullName: 'Ada Lovelace', email: 'adalovelace@mulheres.br' },
+];
+
+const newWoman = women.reduce((acumulador, item) => {
+
+  acumulador[item.fullName] = item.email
+
+  return acumulador
+}, {})
+
+console.log(newWoman);
