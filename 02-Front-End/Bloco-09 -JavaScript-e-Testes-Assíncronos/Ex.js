9f4b36a4d7d2b08e49b7987891c19b7827d573be
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-08-04 11:50:19 -0300

    Exercício de testes assincronos com callbacks

--- 02-Front-End/Bloco-09 -JavaScript-e-Testes-Assíncronos/Ex.js
+++ 02-Front-End/Bloco-09 -JavaScript-e-Testes-Assíncronos/Ex.js
@@ -18,24 +18,56 @@
 
 // 2-
 
-const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
-const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+// const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+// const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+// const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+// const getUser = (callback) => {
+//   setTimeout(() => {
+//     const user = {
+//       firstName: "Ivan",
+//       lastName: "Ivanovich",
+//       nationality: "Russian",
+//     };
+//     // Retorne a `callback` passada como parâmetro na função `getUser`
+//     // Dica: você pode manter o `console.log()`
+
+//     console.log(callback(user));
+//   }, delay());
+// };
+
+// getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+// getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
+
+// 3-
+
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
 
 const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
 
-const getUser = (callback) => {
-  setTimeout(() => {
-    const user = {
-      firstName: "Ivan",
-      lastName: "Ivanovich",
-      nationality: "Russian",
-    };
-    // Retorne a `callback` passada como parâmetro na função `getUser`
-    // Dica: você pode manter o `console.log()`
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
 
-    console.log(callback(user));
+const getCountry = (onSuccess) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      onSuccess(country);
+    } else {
+      const errorMessage = "Country could not be found";
+      return errorMessage
+    }
   }, delay());
 };
 
-getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
-getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
+// Deve imprimir "Returned country is Brazil" no sucesso ou "Error getting country: Country could not be found" em caso de falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em caso de falha
+getCountry(countryCurrency, printErrorMessage);
---
+++ 02-Front-End/Bloco-09 -JavaScript-e-Testes-Assíncronos/Ex.test.js
@@ -0,0 +1,30 @@
+// test('Não deveria passar!', (done) => {
+//   setTimeout(() => {
+//     try {
+//       expect(10).toBe(5);
+//       console.log('Deveria falhar!');
+//       done()
+//     } catch (error) {
+//         done(error)
+//     }
+    
+//   }, 500);
+// });
+
+const asyncSum = (a, b, callback) => {
+  setTimeout(() => {
+    const sum = a + b;
+    callback(sum);
+  }, 500);
+}
+
+test('Testando asyncSum, soma 5 mais 10', (done) => {
+  asyncSum(5, 10, (result) => {
+    try {
+      expect(result).toBe(15);
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});
