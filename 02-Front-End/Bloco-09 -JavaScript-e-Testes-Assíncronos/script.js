2c1f010aa3423594dafd6167d906bad472292aa6
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-08-04 10:33:38 -0300

    Conteudo 1 do course

--- 02-Front-End/Bloco-09 -JavaScript-e-Testes-Assíncronos/script.js
+++ 02-Front-End/Bloco-09 -JavaScript-e-Testes-Assíncronos/script.js
@@ -1,8 +1,59 @@
-const pushNumbers = (list, number) => list.push(number);
+// const pushNumbers = (list, number) => list.push(number);
 
-let numbers = [];
+// let numbers = [];
 
-setTimeout(() => pushNumbers(numbers, 1), 3000)
-pushNumbers(numbers, 1);
+// setTimeout(() => pushNumbers(numbers, 1), 3000)
+// pushNumbers(numbers, 1);
 
-console.log(numbers);
+// console.log(numbers);
+
+// const despesaMensal = (renda, despesas, callback) => {
+
+//   const despesaTotal = callback(despesas);
+//   const saldoFinal = renda - despesaTotal;
+
+//   console.log(`Balanço do mês:
+//     Recebido: R$${renda},00
+//     Gasto: R$${despesaTotal},00
+//     Saldo: R$${saldoFinal},00 `);
+// };
+
+// despesaMensal()
+const despesas = [
+  {
+    gym: 99,
+  },
+  {
+    ifood: 200,
+  },
+  {
+    phone: 60,
+  },
+  {
+    internet: 100,
+  },
+];
+
+const renda = 1000;
+
+const despesaMensal = (renda, despesas, callback) => {
+
+  const despesaTotal = callback(despesas);
+  const saldoFinal = renda - despesaTotal;
+
+  console.log(`Balanço do mês:
+    Recebido: R$${renda},00
+    Gasto: R$${despesaTotal},00
+    Saldo: R$${saldoFinal},00 `);
+};
+
+const somaDespesas = (despesas) => {
+  const custoItem = despesas.map((item) => Object.values(item));
+  const despesaTotal = custoItem.reduce((acc, curr) => acc += curr[0], 0);
+  return despesaTotal;
+};
+
+// acc é a sigla para accumulator (acumulador)
+// curr é a sigla para current (valor atual)
+
+despesaMensal(renda, despesas, somaDespesas);
