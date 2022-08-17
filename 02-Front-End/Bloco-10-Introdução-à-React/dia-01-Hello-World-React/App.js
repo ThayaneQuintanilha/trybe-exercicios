8c7262db845975f178f413a3b6c35e66a2ab1a48
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-08-17 12:53:13 -0300

    Enviando os exercícios do dia 01

---
+++ 02-Front-End/Bloco-10-Introdução-à-React/dia-01-Hello-World-React/App.js
@@ -0,0 +1,19 @@
+
+
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Content from './components/Content';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
