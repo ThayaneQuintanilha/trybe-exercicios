eb95f3cc7afbc37b3331ce2db01f5f515e512e95
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-09-22 15:48:08 -0300

    Combinar vários tipos de reducer para a store

--- 02-Front-End/Bloco-15-Redux/action-reducer-store.js
+++ 02-Front-End/Bloco-15-Redux/action-reducer-store.js
@@ -10,3 +10,16 @@ const reducer = (state = estadoInicial) => {
 const store = Redux.createStore(reducer2);
 
 console.log(store.getState());
+
+// reducer combinado
+
+import { combineReducers } from 'redux';
+import meuReducer from './meuReducer';
+import meuOutroReducer from './meuOutroReducer';
+
+const reducerCombinado = combineReducers({
+  meuReducer,
+  meuOutroReducer,
+})
+
+export default reducerCombinado;
