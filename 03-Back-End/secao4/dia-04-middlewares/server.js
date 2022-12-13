34fb2c01e2e6dd40e8860d0576f32bead32a76a9
thayane <thayaniquintanilha2@gmail.com>
2022-12-13 11:27:02 -0300

    feat: adiciona e implementa lógica do middleware validateDifficulty

--- 03-Back-End/secao4/dia-04-middlewares/server.js
+++ 03-Back-End/secao4/dia-04-middlewares/server.js
@@ -4,8 +4,9 @@ const validatePrice = require('./middlewares/validatePrice');
 const validateDescription = require('./middlewares/validateDescription');
 const validateCreatedAt = require('./middlewares/validateCreatedAt');
 const validateRating = require('./middlewares/validateRating');
+const validateDifficulty = require('./middlewares/validateDifficulty');
 
-app.post('/activities', validateName, validatePrice, validateDescription, validateCreatedAt, validateRating, (req, res) => {
+app.post('/activities', validateName, validatePrice, validateDescription, validateCreatedAt, validateRating, validateDifficulty, (req, res) => {
   res.status(201).json({ message: 'Atividade foi cadastrada com sucesso!'})
 })
 
