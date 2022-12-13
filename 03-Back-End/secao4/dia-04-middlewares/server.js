8717b047eb40ef5163be7b223698d2bc23d8ad50
thayane <thayaniquintanilha2@gmail.com>
2022-12-13 11:20:40 -0300

    feat: adiciona e implementa o middleware validateRating

--- 03-Back-End/secao4/dia-04-middlewares/server.js
+++ 03-Back-End/secao4/dia-04-middlewares/server.js
@@ -3,8 +3,9 @@ const validateName = require('./middlewares/validateName');
 const validatePrice = require('./middlewares/validatePrice');
 const validateDescription = require('./middlewares/validateDescription');
 const validateCreatedAt = require('./middlewares/validateCreatedAt');
+const validateRating = require('./middlewares/validateRating');
 
-app.post('/activities', validateName, validatePrice, validateDescription, validateCreatedAt, (req, res) => {
+app.post('/activities', validateName, validatePrice, validateDescription, validateCreatedAt, validateRating, (req, res) => {
   res.status(201).json({ message: 'Atividade foi cadastrada com sucesso!'})
 })
 
