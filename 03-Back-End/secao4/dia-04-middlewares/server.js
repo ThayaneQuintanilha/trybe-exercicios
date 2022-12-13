5b8dcade568ec12e470e019d461e29daaac206b5
thayane <thayaniquintanilha2@gmail.com>
2022-12-13 11:17:00 -0300

    feat: adiciona e implementa lógica do middleware validateCreatedAt

--- 03-Back-End/secao4/dia-04-middlewares/server.js
+++ 03-Back-End/secao4/dia-04-middlewares/server.js
@@ -2,8 +2,9 @@ const app = require('./app');
 const validateName = require('./middlewares/validateName');
 const validatePrice = require('./middlewares/validatePrice');
 const validateDescription = require('./middlewares/validateDescription');
+const validateCreatedAt = require('./middlewares/validateCreatedAt');
 
-app.post('/activities', validateName, validatePrice, validateDescription, (req, res) => {
+app.post('/activities', validateName, validatePrice, validateDescription, validateCreatedAt, (req, res) => {
   res.status(201).json({ message: 'Atividade foi cadastrada com sucesso!'})
 })
 
