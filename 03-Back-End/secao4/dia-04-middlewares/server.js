594fe00fcacd8c1bea2df058df9406443de05a2c
thayane <thayaniquintanilha2@gmail.com>
2022-12-13 11:12:37 -0300

    feat: adiciona e implementa middleware validateDescription

--- 03-Back-End/secao4/dia-04-middlewares/server.js
+++ 03-Back-End/secao4/dia-04-middlewares/server.js
@@ -1,8 +1,9 @@
 const app = require('./app');
 const validateName = require('./middlewares/validateName');
 const validatePrice = require('./middlewares/validatePrice');
+const validateDescription = require('./middlewares/validateDescription');
 
-app.post('/activities', validateName, validatePrice, (req, res) => {
+app.post('/activities', validateName, validatePrice, validateDescription, (req, res) => {
   res.status(201).json({ message: 'Atividade foi cadastrada com sucesso!'})
 })
 
