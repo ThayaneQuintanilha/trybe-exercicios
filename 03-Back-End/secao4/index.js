df66769351d812cdd17a246b557df8ee92650ea4
thayane <thayaniquintanilha2@gmail.com>
2022-12-13 10:41:59 -0300

    chore: adiciona pacote express

--- 03-Back-End/secao4/index.js
+++
@@ -1,9 +0,0 @@
-const bmi = require('./bmi');
-const readlineSync = require('readline-sync');
-
-const weight = readlineSync.questionInt("What's your weight?");
-const height = readlineSync.questionFloat("What's your height?");
-
-const result = bmi.calculaImc(weight, height).toFixed(2);
-
-console.log(result);
